3f305df9ea593c438db111755b2cc8250f83e604
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-01-31 10:42:12 +0000

    Updated demo.

--- demos/demo2.php
+++ demos/demo2.php
@@ -7,11 +7,15 @@ require '../src/jjok/Flasher/SessionFlasher.php';
 require '../src/jjok/Flasher/Messages/AbstractMessage.php';
 require '../src/jjok/Flasher/Messages/Message.php';
 
-$session = array();
+session_start();
 
-$message_queue = new jjok\Flasher\SessionFlasher($session, 'something');
+$message_queue = jjok\Flasher\SessionFlasher::loadFromSession($_SESSION, 'something');
 $message_queue->enqueue(new Message('This is a message.'));
 
-foreach($message_queue as $message) {
-	echo $message;
+unset($message_queue);
+
+$message_queue = jjok\Flasher\SessionFlasher::loadFromSession($_SESSION, 'something');
+
+while(!$message_queue->isEmpty()) {
+	echo $message_queue->dequeue();
 }
