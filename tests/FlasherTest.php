7797f6d56329e49605a7cee920a954cdea998d28
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-05-08 10:50:08 +0100

    Added some comments to tests.

--- tests/FlasherTest.php
+++ tests/FlasherTest.php
@@ -15,23 +15,23 @@ class FlasherTest extends PHPUnit_Framework_TestCase {
                      ->getMock();
 		
 		$queue = new \jjok\Flasher\Flasher();
-		$this->assertTrue($queue->isEmpty());
+		$this->assertTrue($queue->isEmpty(), 'Queue should start empty.');
 		
 		# Queue message
 		$queue->enqueue($mock_message);
 
-		$this->assertFalse($queue->isEmpty());
-		$this->assertSame(1, $queue->count());
+		$this->assertFalse($queue->isEmpty(), 'Queue should not be empty after message has been queued.');
+		$this->assertSame(1, $queue->count(), 'Queue length should be 1 after queuing 1 message.');
 
 		# Queue message
 		$queue->enqueue($mock_message);
 
-		$this->assertSame(2, $queue->count());
+		$this->assertSame(2, $queue->count(), 'Queue length should be 2 after queuing 2 messages.');
 
 		$this->assertInstanceOf('jjok\Flasher\Messages\AbstractMessage', $queue->dequeue());
-		$this->assertSame(1, $queue->count());
+		$this->assertSame(1, $queue->count(), 'Queue length should be 1 after dequeuing 1 message.');
 
 		$this->assertInstanceOf('jjok\Flasher\Messages\AbstractMessage', $queue->dequeue());
-		$this->assertTrue($queue->isEmpty());
+		$this->assertTrue($queue->isEmpty(), 'Queue should be empty after dequeuing all messages.');
 	}
 }
